2133a7d9f7635e0af1d2ab3cf048938cbf941212
Luna Vulpo <luna.vulpo@gmail.com>
2014-11-08 11:29:23 +0100

    .

--- BoardServer/BoardServer/gameofthrones/boardserver/BoardServer.java
+++ BoardServer/BoardServer/gameofthrones/boardserver/BoardServer.java
@@ -3,8 +3,8 @@ package gameofthrones.boardserver;
 public class BoardServer {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
 
+        System.out.println("init");
     }
 
 }
