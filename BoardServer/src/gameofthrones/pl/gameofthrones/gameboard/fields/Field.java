b45ac0999aa5e26df3ab022dc578c741d057645c
Luna Vulpo <luna.vulpo@gmail.com>
2015-01-10 10:00:54 +0100

    new formatting

--- BoardServer/src/gameofthrones/pl/gameofthrones/gameboard/fields/Field.java
+++ BoardServer/src/gameofthrones/pl/gameofthrones/gameboard/fields/Field.java
@@ -10,52 +10,52 @@ import pl.gameofthrones.gameboard.pieces.Piece;
 /**
  * 
  * pl: pole
+ * 
  * @author arek
- *
+ * 
  */
 public abstract class Field {
-    
-    protected List<Field> neighborFields = new LinkedList<Field>();
-    
-    @Expose
-    private List<Integer> neighborFieldIds = new LinkedList<Integer>();
-    
-    protected List<Piece> pieces = new LinkedList<Piece>();
-    
-    @Expose
-    protected final int id;
-    
-    @Expose
-    protected final String name;
-    
-    @Expose
-    protected int moveCost = 1;
-    
-    /**
-     * 
-     * @param id
-     * @param name
-     */
-     Field(int id, String name){
-        this.id = id;
-        this.name = name;
-    }
 
-    
-    public void addNeighbor(Field field) {
-        neighborFields.add(field);
-        neighborFieldIds.add(field.id);
-    }
+	protected List<Field> neighborFields = new LinkedList<Field>();
 
-    void addPiece(Piece piece){
-    	pieces.add(piece);
-    }
-    
-    boolean removePiece(Piece piece){
-    	return pieces.remove(piece);
-    }
-    
-    int getPiecesCount(){
-    	return pieces.size();
-    }
+	@Expose
+	private List<Integer> neighborFieldIds = new LinkedList<Integer>();
+
+	protected List<Piece> pieces = new LinkedList<Piece>();
+
+	@Expose
+	protected final int id;
+
+	@Expose
+	protected final String name;
+
+	@Expose
+	protected int moveCost = 1;
+
+	/**
+	 * 
+	 * @param id
+	 * @param name
+	 */
+	Field(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public void addNeighbor(Field field) {
+		neighborFields.add(field);
+		neighborFieldIds.add(field.id);
+	}
+
+	void addPiece(Piece piece) {
+		pieces.add(piece);
+	}
+
+	boolean removePiece(Piece piece) {
+		return pieces.remove(piece);
+	}
+
+	int getPiecesCount() {
+		return pieces.size();
+	}
 }
